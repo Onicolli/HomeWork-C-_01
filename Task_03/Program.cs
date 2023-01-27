8b05458b1caca55d2bdcc0085426495d559a19d4
onicolli <onicolli>
2023-01-27 19:28:19 +0600

    Do Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, котороя на вход принимает число и выдает, являеться ли оно четным. 
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!)!;
+if (Num % 2 == 0) {
+    Console.WriteLine("Число " + Num + " Да");
+}
+else {
+    Console.WriteLine("Число " + Num + " Нет");
+}
