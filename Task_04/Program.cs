ad391600bef670b771e78add2162219e33633da2
onicolli <onicolli>
2023-01-27 19:33:26 +0600

    Do Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, котороя на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!)!;
+int Arr = 0;
+while (Arr < Num) {
+    Arr += 1;
+    Console.WriteLine(Arr);
+}
+
